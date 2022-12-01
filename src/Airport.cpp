767af611c8e826129b3b8325447f7133ad3ef9c8
BOOOOOOOO <92756272+boyuann@users.noreply.github.com>
2022-11-30 18:37:13 -0600

    airport
    aa

--- src/Airport.cpp
+++ src/Airport.cpp
@@ -1,1 +1,52 @@
-// placeholder
+#include "Airport.h"
+
+Airport::Airport(){
+    id_ = 0;
+    name_ = "";
+    city_ = "";
+    latitude_ = 0.0;
+    longitude_ = 0.0;
+
+}
+//copy constructor
+Airport::Airport(int id, std::string name, std::string city, std::string country, double latitude, double longitude) {
+    id_ = id;
+    name_ = name;
+    city_ = city;
+    country_ = country;
+    latitude_ = latitude;
+    longitude_ = longitude;
+}
+
+Airport::Airport(const Airport& other){
+    id_ = other.id_;
+    name_ = other.name_;
+    city_ = other.city_;
+    latitude_ = other.latitude_;
+    longitude_ = other.longitude_;
+}
+
+bool Airport::operator==(const Airport & other) const{
+    if(id_ == other.id_ && name_ == other.name_ && city_ == other.city_ && latitude_ == other.latitude_ && longitude_ == other.longitude_) {
+      return true;
+    }
+    return false;
+
+}
+
+Airport Airport::operator=(const Airport & other) {
+    id_ = other.id_;
+    name_ = other.name_;
+    city_ = other.city_;
+    latitude_ = other.latitude_;
+    longitude_ = other.longitude_;
+    return *this;
+}
+
+int Airport::getID() {
+    return id_;
+}
+
+std::string Airport::getName() {
+    return name_;
+}
--- src/Airport.h
+++ src/Airport.h
@@ -2,9 +2,21 @@
 
 #include <string>
 
-struct Airport {
-  unsigned int id;
-  std::string name;
-  double latitude;
-  double longitude;
+class Airport {
+  private:
+    unsigned int id_;
+    std::string name_;
+    std::string city_;
+    std::string country_;
+    double latitude_;
+    double longitude_;
+  public:
+    Airport();
+    Airport(int id, std::string name, std::string city, std::string country_, double latitude, double longitude);
+    Airport(const Airport& other);
+    int getID();
+    std::string getName();
+    std::string getCity();
+    bool operator==(const Airport & other) const;
+    Airport operator=(const Airport & other);
 };
