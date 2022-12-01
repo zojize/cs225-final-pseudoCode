011eb6a0681e2406fd9e616fa8a91db2f717b46f
SIGUSR97 <jeffzo01@qq.com>
2022-12-01 16:36:59 -0600

    feat: bfs_walk

--- src/Algorithms.h
+++ src/Algorithms.h
@@ -2,29 +2,56 @@
 #include "Graph.h"
 #include "Route.h"
 
-using airport_id = unsigned int;
+#include <unordered_map>
+#include <utility>
 
 namespace Algorithms {
+
+enum TraversalLabel {
+  UNEXPLORED,
+  VISITED,
+  DISCOVERY,
+  CROSS,
+  BACK,
+};
+
+template <typename T>
+using Labels =
+    std::pair<std::unordered_map<T, TraversalLabel>,
+              std::unordered_map<T, std::unordered_map<T, TraversalLabel>>>;
+
+template <typename T>
+TraversalLabel get_label(Labels<T>& labels, T vertex);
+template <typename T>
+TraversalLabel get_label(Labels<T>& labels, T source, T destination);
+template <typename T>
+TraversalLabel get_label(Labels<T>& labels, typename Graph<T>::Edge edge);
+
+template <typename T>
+void bfs_walk(Graph<T> const& g, Labels<T>&);
+
 /**
  * Get the shortest path from source to destination using A*
  *
- * @param g  
- * @param source 
- * @param destination 
+ * @param g
+ * @param source
+ * @param destination
  * @returns vector of Route
  */
-std::vector<Route> find_shortest_path_A_star(Graph<airport_id> &g,
-                                             airport_id source,
-                                             airport_id destination);
+std::vector<Route> find_shortest_path_A_star(Graph<Airport>& g, Airport source,
+                                             Airport destination);
+
 /**
  * Get the shortest path from source to destination using dijkstra
  *
- * @param g 
- * @param source 
- * @param destination 
+ * @param g
+ * @param source
+ * @param destination
  * @returns vector of Route
  */
-std::vector<Route> find_shortest_path_dijkstra(Graph<airport_id> &g,
-                                               airport_id source,
-                                               airport_id destination);
+std::vector<Route> find_shortest_path_dijkstra(Graph<Airport>& g,
+                                               Airport source,
+                                               Airport destination);
 }; // namespace Algorithms
+
+#include <Algorithms.hpp>
